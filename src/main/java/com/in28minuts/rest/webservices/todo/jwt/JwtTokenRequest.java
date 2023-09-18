9fecac3aaebe492569989971a0127d510563226f
admin-ido <ido.de.haan@live.nl>
2023-09-18 18:12:51 +0000

    add JWT authentication classes (user still hard-coded in jwt-config)

---
+++ src/main/java/com/in28minuts/rest/webservices/todo/jwt/JwtTokenRequest.java
@@ -0,0 +1,4 @@
+package com.in28minuts.rest.webservices.todo.jwt;
+
+public record JwtTokenRequest(String username, String password) {
+}
---
+++ src/main/java/com/in28minuts/rest/webservices/todo/jwt/JwtTokenResponse.java
@@ -0,0 +1,4 @@
+package com.in28minuts.rest.webservices.todo.jwt;
+
+public record JwtTokenResponse(String token) {
+}
